668144722ee15522c1508b42c9273424d52474c0
xamdacilmi@gmail.com <xamdacilmi@gmail.com>
2022-07-12 14:40:38 -0700

    added main.h,_putchar,print_string

---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "stdarg.h"
+#include "main.h"
+
+/* i - is the number that we will enter/type or other wise a vlaue.*/
+/*n - is the count, it counts the number of characters are the printed data.*/
+
+
+
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
+ */
+int _printf(const char *format, ...)
+{
+	int n = 0, i = 0;
+
+	va_list ap;
+
+	va_start(ap, format);
+
+	for (i = 0; format[i] != '%' && format[i] != '\0'; i++)
+	{
+
+		if (format[i] != '%')
+		{
+			n += _putchar(format[i]);
+			i++;
+		}
+		else if (format[i + 1] == '%' && format[i + 1] != ' ')
+		{
+		switch (format[i + 1])
+		{
+			case 's':
+			n += print_str(va_arg(ap, char *));
+			break;
+			case 'c':
+			n += _putchar(va_arg(ap, int));
+			break;
+			case '%':
+			n += _putchar('%');
+			break;
+			default:
+			break;
+		}
+		}
+	}
+	va_end(ap);
+
+	return (n);
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = printf("%s", "Hello");
+	int a = printf("%c", 'H');
+	int b = printf("%%");
+
+	printf("\n%d\n", n);
+	printf("\n%d\n", a);
+	printf("\n%d\n", b);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_str(char *);
+int print_dec(int n);
+
+#endif
