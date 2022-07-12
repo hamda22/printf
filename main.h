5879e591d2366288527284c0b0b24241c8efb2eb
Mohammad <mohaomar495@gmail.com>
2022-07-12 15:39:52 -0700

    updated

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
 #include <unistd.h>
+
+
 
 /**
  * struct format - match the conversion specifiers for printf
